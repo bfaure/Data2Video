3c13e96f0bb4da88358bed8111dc3eae991221f0
Brian Faure <bfaure23@gmail.com>
2017-08-31 00:28:21 -0400

    pipeline now fully works

--- main.py
+++ main.py
@@ -13,14 +13,15 @@ HD = (1920,1080)
 
 # provided a list of pixels, writes it out as an image
 # with the specified resolution
-def pixels_2_png(pixels,reso=four_k,fname="image.png"):
+def pixels_2_png(pixels,fname,reso=four_k):
 	img = Image.new('RGB',reso)
 	img.putdata(pixels)
 	img.save(fname)
-	print "Saved to %s" % fname
+	#print pixels[:16]
+	print "pixels_2_png: Saved to %d pixels to %s" % (len(pixels),fname)
 
 # provided a filename, reads the png and returns a list of pixels
-def png_2_pixels(fname="image.png"):
+def png_2_pixels(fname):
 	im = Image.open(fname)
 	pixel_list = []
 	pixels = im.load()
@@ -28,6 +29,8 @@ def png_2_pixels(fname="image.png"):
 	for row in range(height):
 		for col in range(width):
 			pixel_list.append(pixels[col,row])
+	print "png_2_pixels: Read %d pixels from %s" % (len(pixel_list),fname)
+	#pixels_2_png(pixel_list,"test2.png")
 	return pixel_list
 
 # returns a list of bits in the file
@@ -41,26 +44,30 @@ def file_2_bits(fname):
 	bits = []
 	for b in bit_reader(open(fname,'r')):
 		bits.append(b)
+	print "file_2_bits: Read %d bits from %s" % (len(bits),fname)
 	return bits
 
 # converts a list of 0/1 bits to pixels
 def bits_2_pixels(bits):
 	pixels=[]
 	for b in bits:
-		if b==0:
+		if b=='0':
 			pixels.append((0,0,0))
 		else:
 			pixels.append((255,255,255))
+	print "bits_2_pixels: Converted %d bits to %d pixels" % (len(bits),len(pixels))
 	return pixels 
 
 # converts opposite of bits_2_pixels
 def pixels_2_bits(pixels):
+	#print pixels[:16]
 	bits = []
 	for p in pixels:
 		if p==(0,0,0):
 			bits.append('0')
 		else:
 			bits.append('1')
+	print "pixels_2_bits: Converted %d pixels to %d bits" % (len(pixels),len(bits))
 	return bits
 
 # adds the file name header to the list of bits
@@ -70,6 +77,8 @@ def add_header(bits,fname):
 	
 	# extra 2 bytes (16 bits) before header tells how long header is (in bits)
 	length_header = "{0:b}".format(len(header_bits))
+
+	print "header_length: %s" % len(header_bits)
 
 	while len(length_header)<16:
 		length_header = "0"+length_header
@@ -85,44 +94,59 @@ def add_header(bits,fname):
 
 	# append the original bits onto the header and return
 	header_list.extend(bits)
+	print "add_header: Added %d length header, total bits: %d" % (len(total_header),len(header_list))
+	#print header_list[:16]
 	return header_list
+
+def decode_binary_string(s):
+    return ''.join(chr(int(s[i*8:i*8+8],2)) for i in range(len(s)//8))
 
 # takes in the bits, decodes the header into a filename.
 # returns the filename, as well as the rest of the bits 
 # after the header section
 def decode_header(bits):
 	header_length_binstr = ''.join(bits[:16])
+	#print header_length_binstr
 	header_length = int(header_length_binstr,2)
-	
-	rest_of_header = ''.join(bits[16:16+header_length])
-	n = int(rest_of_header,2)
-	fname = binascii.unhexlify('%x' % n)
+	print "header_length: %d" % header_length
 
+	rest_of_header = ''.join(bits[16:16+header_length])
+	rest_of_header = "0"+rest_of_header
+	print rest_of_header
+	fname = decode_binary_string(rest_of_header)
+	#n = int(rest_of_header,2)
+	#fname = binascii.unhexlify('%x' % n)
+
+	print "decode_header: Found %d length header, " % (header_length)
 	return fname,bits[16+header_length:]
 
 def main():
 
-	test_f = "data/test.txt"
+	src_f 		= "data/b.txt"
+	src_f_cln 	= "b.txt"
+	img_f 		= "data/image.png"
 
-	print "Converting file to bits..."
-	test_f_bits = file_2_bits(test_f)
 
-	print "Adding header to bits..."
-	test_f_bits = add_header(test_f_bits,test_f.split("/")[1])
+	#print "Converting file to bits..."
+	test_f_bits = file_2_bits(src_f)
 
-	print "Converting bits to pixels..."
+	#print "Adding header to bits..."
+	test_f_bits = add_header(test_f_bits,src_f_cln)
+
+	#print "Converting bits to pixels..."
 	pixels = bits_2_pixels(test_f_bits)
 
-	print "Writing out pixels to image..."
-	pixels_2_png(pixels)
+	#print "Writing out pixels to image..."
+	pixels_2_png(pixels,img_f)
 
-	print "Reading pixels from png..."
-	pixels = png_2_pixels("image.png")
 
-	print "Converting pixels to bits..."
+	#print "Reading pixels from png..."
+	pixels = png_2_pixels(img_f)
+
+	#print "Converting pixels to bits..."
 	bits = pixels_2_bits(pixels)
 
-	print "Decoding bit header..."
+	#print "Decoding bit header..."
 	fname,bits = decode_header(bits)
 
 	print "Recovered filename: %s" % fname 
